f81ccb7a08ef7a8296738ef252793a509d8b7e2d
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-06-10 21:21:25 +0530

    first commit

---
+++ Chapter-1/index.ts
@@ -0,0 +1,14 @@
+// Type Script:- A Developer Tool/Dependency Which Helps Us Write Code Safely
+// Types:-
+    // Number
+    // String
+    // Boolean
+    // Null
+    // Void
+    // Undefined
+    // Object
+    // Array
+    // Tuples
+    // Any
+    // Unknown
+    // Never
